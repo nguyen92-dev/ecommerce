d999c75ec97f8e816ba83a2c2b5238042b26a57e
nguyen92-dev <nguyenthanhthao@192.168.0.165.non-exists.ptr.local>
2024-11-16 15:14:57 +0700

    initial navbar and header

--- apps/ecom-frontend/src/app/layout/navbar/navbar.component.ts
+++ apps/ecom-frontend/src/app/layout/navbar/navbar.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'ecom-navbar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink, FaIconComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
--- apps/ecom-frontend/src/app/shared/font-awesome-icons.ts
+++ apps/ecom-frontend/src/app/shared/font-awesome-icons.ts
@@ -1,6 +1,12 @@
 import { IconDefinition } from '@fortawesome/angular-fontawesome';
-import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { faCartShopping, faTruckFast, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faFacebook, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons';
 
 export const fontAwesomeIcons: IconDefinition[] = [
-  faUser
+  faUser,
+  faCartShopping,
+  faTruckFast,
+  faFacebook,
+  faTwitter,
+  faYoutube
 ]
