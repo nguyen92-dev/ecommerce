b3412ed64f5540df2a2f97ce1f7260ca8d39228b
nguyennd <nguyen32.nd@gmail.com>
2024-11-18 19:05:12 +0700

    initial backend and database

---
+++ apps/ecom-backend/src/test/java/top/nguyennd/ecom/EcomBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package top.nguyennd.ecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
